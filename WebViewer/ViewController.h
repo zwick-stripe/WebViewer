b5bdb89f308fa993f1b5b5e304a2fe364e8915d3
Zach Wick <zwick@stripe.com>
2017-03-02 11:07:51 -0500

    Updates licensing info in the source files
    Signed-off-by: Zach Wick <zwick@stripe.com>

--- WebViewer/ViewController.h
+++ WebViewer/ViewController.h
@@ -3,7 +3,8 @@
 //  WebViewer
 //
 //  Created by Zach Wick on 2017-03-02.
-//  Copyright © 2017 zach wick. All rights reserved.
+//  Copyright © 2017 zach wick
+//  Licensed under the GNU GPLv3 or later
 //
 
 #import <UIKit/UIKit.h>
